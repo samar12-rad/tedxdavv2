7d7e7e296e085c74706db7b6164415a3ffdb53be
Nishant Kaushal <nishantkaushal0708@gmail.com>
2024-02-11 23:27:06 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import "./App.css"
+import Navbar from "./pages/Navbar/Navbar"
+import Ticket from './pages/Ticket/Ticket';
+import Footer from './pages/Footer/Footer';
+
+
+
+
+function App() {
+
+ 
+
+  return (
+    <div  className=''>
+     <Navbar/>
+     <Ticket/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
